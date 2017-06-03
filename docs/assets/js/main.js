64eae45b91c7bbb7bc2afaced4085b59a2a11088
Shoya Ishimaru <shoya.ishimaru@gmail.com>
2017-06-03 23:02:59 +0200

    Deleted Hatena Star

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -49,9 +49,6 @@ $(function(){
   });
 
 
-  // hatena ster
-  Hatena.Star.EntryLoader.headerTagAndClassName = ['div','hatena-star'];
-
   // image fade
   $(".image-fade").css('visibility','visible').hide().fadeIn(800);
 
